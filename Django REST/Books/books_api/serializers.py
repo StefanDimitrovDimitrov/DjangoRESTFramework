d457ead2a4a0fafc245ee9138427bae33457f326
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-08-24 11:19:45 +0300

    add custom validation to serializer

--- Django REST/Books/books_api/serializers.py
+++ Django REST/Books/books_api/serializers.py
@@ -3,6 +3,18 @@ from .models import BookModel
 
 
 class BookSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        title = data['title']
+        if title:
+            start_letter = title[0]
+            if start_letter.islower():
+                raise serializers.ValidationError("Must start with capital letter")
+            return data
+
+
+
+
     class Meta:
         model = BookModel
         fields = "__all__"
